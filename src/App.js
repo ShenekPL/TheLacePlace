580383d006a8bc098748b0b7b38d0427ac57c93d
ShenekPL <90441718+ShenekPL@users.noreply.github.com>
2025-04-23 14:53:37 +0200

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { React, useState } from "react";
+import TextField from "@mui/material/TextField";
+import List from "./Components/List"
 import './App.css';
-
+import logo from './Logo.png'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <section className="Section">
-          <p>Piddy</p>
+          <img src={logo} alt="Logo" className="Logo" />
+          <div className="search">
+            <TextField
+              id="outlined-basic"
+              variant="outlined"
+              fullWidth
+              label="Search"
+            />
+      </div>
         </section>
         <section className="Section2">
           <p>Diddy</p>
