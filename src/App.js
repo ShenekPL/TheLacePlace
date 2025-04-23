e135b9c2df888b7799146cc152bd36d9193d6166
ShenekPL <90441718+ShenekPL@users.noreply.github.com>
2025-04-23 14:12:59 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <section className="Section">
+          <p>Piddy</p>
+        </section>
+        <section className="Section2">
+          <p>Diddy</p>
+        </section>
+      </header>
+    </div>
+  );
+}
+
+export default App;
